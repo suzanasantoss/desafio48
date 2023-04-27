48e3744ee3565208312a1bf6dc708e7b7fb43ef8
suzanasantoss <126722205+suzanasantoss@users.noreply.github.com>
2023-04-27 20:12:53 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+/*
+    -->Primeiro, todas as letras minúsculas e maiúsculas são deslocadas três posições para a direita, seguindo a tabela ASCII. 
+    -->Por exemplo, a letra 'a' vira 'd', a letra 'y' vira '|', e assim por diante.
+    -->Depois, a linha é invertida.
+    -->Por fim, os caracteres a partir da metade são deslocados uma posição para a esquerda na tabela ASCII. Por exemplo, 'b' vira 'a' e 'a' vira '`'.
+*/
+/*-------------------------------------------------------------*/
+
+let submit = document.getElementById('submit');
+submit.addEventListener/*("click", convert);*/
+
+$(document).ready(function() {
+
+});
+
+
+
+
+/*-------2ªFUNÇÃO----------------------------------------------*/
+
+function invertLine(message) {
+  var newMessage = "";
+  for (var i = message.length - 1; i >= 0; i--) { }
