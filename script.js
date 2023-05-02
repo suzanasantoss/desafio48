9e74011a3f0234044adbc79b0b6b1f31dd92c68b
Suzana <126722205+suzanasantoss@users.noreply.github.com>
2023-05-02 19:56:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,73 @@
-/*
-    -->Primeiro, todas as letras minúsculas e maiúsculas são deslocadas três posições para a direita, seguindo a tabela ASCII. 
-    -->Por exemplo, a letra 'a' vira 'd', a letra 'y' vira '|', e assim por diante.
-    -->Depois, a linha é invertida.
-    -->Por fim, os caracteres a partir da metade são deslocados uma posição para a esquerda na tabela ASCII. Por exemplo, 'b' vira 'a' e 'a' vira '`'.
-*/
-/*-------------------------------------------------------------*/
+/*--- Seleciona o botão de envio e adiciona um callback chamando a função treePositionForward assim que o botão é clicado ---*/
+let submit = document.querySelector("#submit");
+submit.addEventListener('click', () => treePositionForward(document.querySelector('#entrada').value));
 
-let submit = document.getElementById('submit');
-submit.addEventListener/*("click", convert);*/
+/*--- Função  que codifica a mensagem de entrada adicionando 3 ao código ASCII de cada caractere imprimível ---*/
+function treePositionForward(msgInput) {
+  let newStr = "";
 
-$(document).ready(function() {
+  for (let i = 0; i < msgInput.length; i++) {
+    let order;
+    if (msgInput[i] != " " && ((msgInput.charCodeAt(i) >= 65 && msgInput.charCodeAt(i) <= 90) || (msgInput.charCodeAt(i) >= 97 && msgInput.charCodeAt(i) <= 122))) {
+      //verificando se é espaço em branco
+      order = msgInput.charCodeAt(i) + 3 >= 32 && msgInput.charCodeAt(i) + 3 <= 126 ? msgInput.charCodeAt(i) + 3 : 32;
+    }else{
+      order = msgInput.charCodeAt(i);
+    }
+    newStr += String.fromCharCode(order);
+  }
+  /*--- Inverte a linha codificada e chama a função shiftLeft com a string invertida como argumento ---*/
+  resultado = invertLine(newStr);
+  shiftLeft(resultado);
 
-});
+};
 
 
+/*---Função que inverte uma string ---*/
+function invertLine(msg) {
+  return msg.split("").reverse().join("");
+}
 
 
-/*-------2ªFUNÇÃO----------------------------------------------*/
+/*--- Função que desloca os caracteres a partir da metade para a esquerda ---*/
+function shiftLeft(texto) {
+  let halfText = Math.floor(texto.length / 2);
+  let shiftedStr = "";
+  for (let i = 0; i < texto.length; i++) {
+    let cod = texto.charCodeAt(i);
+    if (i >= halfText) {
+      cod -= 1;
+    }
+    if (cod >= 32 && cod <= 126) {
+      shiftedStr += String.fromCharCode(cod);
+    } else {
+      shiftedStr += String.fromCharCode(32);
+    }
+  }
 
-function invertLine(message) {
-  var newMessage = "";
-  for (var i = message.length - 1; i >= 0; i--) { }
+  /*--- Exibe o resultado em uma animação de digitação e oculta o loader após 1,5 segundos ---*/
+  loader(shiftedStr);
+}
+
+/*--- Função que escreve o resultado da decodificação no campo de resultado, com animação de digitação ---*/
+function writeResult(resultado) {
+  new TypeIt("#resultado", {
+    strings: `<span class="result">${resultado}</span>`,
+    speed: 85
+  }).go();
+}
+
+/*--- Função para exibir o loader, onde remove o resultado anterior (se houver) e chama a função writeResult após 1,5 segundos ---*/
+function loader(resultado) {
+  removeAnteriorMsg();
+  document.querySelector(".custom-loader").style = "display: block";
+  setTimeout(() => { document.querySelector(".custom-loader").style = "display: none" }, 1500);
+  setTimeout(() => writeResult(resultado), 1500);
+}
+
+/*--- Função que remove o resultado anterior (se houver) ---*/
+function removeAnteriorMsg() {
+  if (document.querySelector(".result")) {
+    document.querySelector(".result").remove();
+  }
+}
